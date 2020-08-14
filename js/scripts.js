0a28c8aba8895f2cb2d47d4ec1010f7e5af76b24
Jillian Gibson <jillianlgibson@gmail.com>
2020-08-13 22:27:11 -0700

    fix error else if output error on color and location
    co-authored by: David McCaslin <davidmccaslin94@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,38 @@
 // user interface
 $(document).ready(function() {
+  console.log("Script executing!")
   $("form#profileInfo").submit(function(event) {
+    console.log("Submit form successfully reached.");
     event.preventDefault();
     let age = $("#age").val();
-    let color = $("#location").val();
-    let location = $("location").val();
+    let color = $("#color").val();
+    let location = $("#location").val();
 
     $(".flounder, .merlin, .cruella, .ursala, .jafar, .pocahontas").hide();
 
     if (age) {   
       if (age < 18) {
-        $(".flounder, .matchTitle").show(1000);  
+        $(".flounder, .matchTitle").show();  
         }
         else if (age > 65) {
-          $(".merlin, .matchTitle").show(1000);    
+          $(".merlin, .matchTitle").show();    
         }
-        else if (age >= 18 && age < 25 || color === "red" && location === "city") {
-          $(".cruella, .matchTitle").show(1000);  
+        else if (color === "Red" && location === "City") {
+          $(".cruella, .matchTitle").show();  
         }
-        else if (age >= 18 && age < 25 || color === "purple" && location === "ocean") {
-          $(".ursala, .matchTitle").show(1000);  
+        else if (color === "Purple" && location === "Ocean") {
+          $(".ursala, .matchTitle").show();  
         }
-        else if (age >= 18 && age < 25 || color === "black" && location === "desert") {
-          $(".jafar, .matchTitle").show(1000);  
+        else if (color === "Black" && location === "Desert") {
+          $(".jafar, .matchTitle").show();  
         }
         else {
-          $(".pocahontas, .matchTitle").show(1000);
+          $(".pocahontas, .matchTitle").show();
         }      
       }
       else {
         alert("Please enter your age");
       }
+      
   });
 });
